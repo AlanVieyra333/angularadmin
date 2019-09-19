41cce9e2d8e969f806c9a532b0aa24fc8a74e8d0
Alan Fernando Rincón Vieyra <alan.vieyra376@gmail.com>
2019-09-19 01:16:01 -0500

    Se modificaron las rutas

--- src/app/tables/tables-routing/tables-routing.module.ts
+++ src/app/tables/tables-routing/tables-routing.module.ts
@@ -3,20 +3,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [{
-    path: '',
-    pathMatch: 'full',
-    component: TablesComponent,
-    data: {
-      title: 'Tables Works'
-    }
-  },{
-    path: 'subpage',
-    pathMatch: 'full',
-    component: TablesComponent,
-    data: {
-      title: 'Subpage Tables Works'
-    }
-  }];
+  path: '',
+  pathMatch: 'full',
+  component: TablesComponent,
+  data: {
+    title: 'Cursos Realizados'
+  }
+}, {
+  path: 'realidad-virtual',
+  pathMatch: 'full',
+  component: TablesComponent,
+  data: {
+    title: 'Realidad Virtual'
+  }
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
